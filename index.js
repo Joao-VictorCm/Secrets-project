7fc322c53daa63b301c0dd784da11ed4cbba5e36
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-10-28 22:28:59 -0300

    Finalizado projeto

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-// DICAS:
-// 1. Importar expresso e axios
-// 2. Crie um aplicativo expresso e defina o número da porta.
-// 3. Use a pasta pública para arquivos estáticos.
-// 4. Quando o usuário acessa a página inicial ele deve renderizar o arquivo index.ejs.
-// 5. Use axios para obter um segredo aleatório e passe-o para index.ejs para exibir o
-// segredo e o nome de usuário do segredo.
-// 6. Ouça na sua porta predefinida e inicie o servidor.
-
-
 import express from "express"
 import axios from "axios"
 
